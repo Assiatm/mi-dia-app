c63f6c7d406587f4f500e5ee73480af57ce0a959
Tatiana <assiashopco@gmail.com>
2026-07-22 08:28:43 -0500

    Actualiza diseño visual, login con PIN, pestañas Notas y Recordatorios

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mi-dia-v2';
+const CACHE = 'mi-dia-v4';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
