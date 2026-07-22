9bcd9c5ddf3bdbb6291dc73c5a142beb151d9bf9
Tatiana <assiashopco@gmail.com>
2026-07-21 21:17:42 -0500

    Sin voz ni Calendar, agrega Control de Viajes y sync con Luttia n8n

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mi-dia-v1';
+const CACHE = 'mi-dia-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
